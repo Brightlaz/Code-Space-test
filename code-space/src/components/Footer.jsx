b38b40410818fc2d8f3b3683f9f9683e23be4720
Bright Lazarus <lazbright1@gmail.com>
2023-05-03 18:56:21 +0100

    Feedbacks implemented, added links and reduced the scroll bar

--- code-space/src/components/Footer.jsx
+++ code-space/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import smarklas from "../images/SmartKlas C2_Websit Header Logo 1.svg";
 import facebook from "../images/facebook.svg";
-import tiktok from "../images/tik_tok.svg";
+import linkedIn from "../images/linkedIn.svg";
 import twitter from "../images/twitter.svg";
 import instagram from "../images/instagram.svg";
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center sm:justify-start flex-col m-10 text-center sm:text-left items-center sm:items-start sm:w-96 md:ms-20 lg:ms-24">
+    <footer className="flex flex-col items-center justify-center m-10 text-center sm:justify-start sm:text-left sm:items-start sm:w-96 md:ms-20 lg:ms-24">
       <p className="mb-4" id="at">
         © 2023 Code Space Africa.
       </p>
@@ -17,10 +17,38 @@ const Footer = () => {
       </p>
       <img className="h-6 mb-10" src={smarklas} alt="logo" />
       <div className="flex justify-between w-9/12 mb-12">
-        <img className="h-10" src={facebook} alt="facebook" />
-        <img className="h-10" src={twitter} alt="twitter" />
-        <img className="h-10" src={instagram} alt="instagram" />
-        <img className="h-10" src={tiktok} alt="tiktok" />
+        <a
+          href="https://m.facebook.com/groups/946095563299608/?ref=share"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-transform duration-300 hover:scale-110"
+        >
+          <img className="h-10" src={facebook} alt="facebook" />
+        </a>
+        <a
+          href="https://twitter.com/CodeSpaceHQ"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-transform duration-300 hover:scale-110"
+        >
+          <img className="h-10" src={twitter} alt="twitter" />
+        </a>
+        <a
+          href="https://www.instagram.com/codesspace/"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-transform duration-300 hover:scale-110"
+        >
+          <img className="h-10" src={instagram} alt="instagram" />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/codespaceafrica"
+          target="_blank"
+          rel="noreferrer"
+          className="transition-transform duration-300 hover:scale-110"
+        >
+          <img className="h-10" src={linkedIn} alt="linkedIn" />
+        </a>
       </div>
     </footer>
   );
